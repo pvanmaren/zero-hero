43a01073ca4d40d835159a971b1a54a4602e508d
Gianni <302325196@student.rocmondriaan.nl>
2023-09-22 12:42:39 +0200

    some minor details changed

--- Assets/Scripts/LoadPersonalInfo.cs
+++ Assets/Scripts/LoadPersonalInfo.cs
@@ -5,49 +5,14 @@ using TMPro;
 public class LoadPersonalInfo : MonoBehaviour
 {
     public AppData appData;
-    private string JSONpath;
     [SerializeField] private TMP_Text fullName;
     [SerializeField] private TMP_Text location;
     [SerializeField] private TMP_Text function;
     [SerializeField] private TMP_Text totalRides;
-
-    [System.Serializable]
-    //Object van een enkele user
-    private class User
-    {
-        public int id;
-        public string email;
-        public string password;
-        public string name;
-        public string location;
-        public string function;
-        public int totalKm;
-        public int totalRides;
-    }
-    [System.Serializable]
-    private class Users
-    {
-        //array van users
-        public User[] users;
-    }
-
     public void ShowPersonalInfo()
     {
-        int userId = appData.GetLoginId() - 1;
-        print(userId);
-        JSONpath = Application.dataPath + "/credentials/users.json";
-        string JSONstring = File.ReadAllText(JSONpath);
-        Users users = JsonUtility.FromJson<Users>(JSONstring);
-        User user = users.users[userId];
-
-        fullName.text = user.name;
-        location.text = user.location;
-        function.text = user.function;
-        totalRides.text = user.totalRides.ToString();
-
+        fullName.text = appData.GetUserFullName();
+        function.text = appData.GetUserFunction();
+        totalRides.text = "0";// Here comes the amount of reservations
     }
-
-
-
-    
 }
